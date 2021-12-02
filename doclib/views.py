4c2a34f21823b3a4a9a252c114d99577bd8c9115
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-12-02 14:29:18 +0530

    Formatted views.py

--- doclib/views.py
+++ doclib/views.py
@@ -13,23 +13,26 @@ from django.core.files.storage import FileSystemStorage
 from doclib.scripts import google_drive_gcs, awsS3
 import requests, os
 from django.conf import settings
+
 ###################
+
 '''
 Parameters for Credentials for configuration
 '''
 base_dir = settings.BASE_DIR
 media_dir = settings.MEDIA_ROOT
 
-storage_chosen = None #to know the storage service, user selected
+storage_chosen = None   # to know the storage service, user selected
 
-bucket_received=None #to know the bucket of storage service selected
-object_token=None #to know the path in storage service where docs to be uploaded
+bucket_received=None    # to know the bucket of storage service selected
+object_token=None       # to know the path in storage service where docs to be uploaded
 
 access_id_received=None
 secret_key_received=None
 
 gcs_client_json_file_name=None
-##################
+
+###################
 
 def index(request):
     '''
@@ -45,8 +48,8 @@ class get_gcs_credentials(APIView):
     if user has selected it as a storage service.
     '''
     def get(self, request):
+        # rendering HTML Form Page for getting credentials of google cloud bucket
         return render(request, "gcs.html")
-        #rendering HTML Form Page for getting credentials of google cloud bucket
 
     def post(self, request):
         try:
@@ -55,11 +58,12 @@ class get_gcs_credentials(APIView):
             f = request.FILES.get("files")
             fs = FileSystemStorage()
             file = fs.save(f.name, f)
-            #saving the gcs bucket credentials file locally in media directory of the project
+            # saving the gcs bucket credentials file locally in media directory of the project
             globals()["gcs_client_json_file_name"] = file
             globals()["storage_chosen"] = "gcs"
+            # redirecting to files display page
             return redirect("/doclib/files_display")
-            #redirecting to files display page
+            
         except Exception as e:
             return "error"
 
@@ -70,9 +74,9 @@ class get_s3_credentials(APIView):
     if user has selected it as a storage service.
     '''
     def get(self, request):
+        # rendering HTML Form Page for getting credentials of amazon s3 bucket
         return render(request, "aws_s3.html")
-        #rendering HTML Form Page for getting credentials of amazon s3 bucket
-
+        
     def post(self, request):
         try:
             globals()['bucket_received'] = request.POST.get('bucket')
@@ -80,9 +84,9 @@ class get_s3_credentials(APIView):
             globals()['secret_key_received'] = request.POST.get('secret_key')
             globals()['object_token'] = request.POST.get('object_token')
             globals()["storage_chosen"] = "s3"
+            # redirecting to files display page
             return redirect("/doclib/files_display")
-            #redirecting to files display page
-
+            
         except Exception as e:
             print(e)
 
@@ -96,14 +100,13 @@ def google_api(request):
     For more details view scripts/google_drive_gcs.py
     '''
     file_details = json.loads(request.body)
-    #getting metadata of selected files
-
+    # getting metadata of selected files
     google_drive_gcs.googleAPI().locally_download_files(file_details)
-    #calling using google drive service to download selected files
+    # calling using google drive service to download selected files
     serializer=DocSerializer(data=file_details,many=True)
     if serializer.is_valid():
         serializer.save()
-        #saving metadata of selected files in models
+        # saving metadata of selected files in models
     return JsonResponse({"status": "Success"})
 
 @csrf_exempt
@@ -117,10 +120,8 @@ def fetch_credentials_gcs(request):
     data["developerKey"] = settings.DEVELOPER_KEY
     data["clientId"] = settings.CLIENT_ID
     data["appId"] = settings.APP_ID
+    # sending google API client credentials received from environment variables to google_picker.js in json format
     return JsonResponse(data)
-    #sending google API client credentials received from environment variables to google_picker.js in json format
-
-
 
 class local_api(APIView):
     '''
@@ -131,7 +132,7 @@ class local_api(APIView):
     def get(self, request):
         form = DocSerializer()
         return render(request, 'local_source.html')
-        #rendering HTML Form Page for uploading local files
+        # rendering HTML Form Page for uploading local files
 
     def post(self, request, *args, **kwargs):
         all_local_files = []
@@ -140,17 +141,14 @@ class local_api(APIView):
         for f in request.FILES.getlist("files"):
             file = fs.save(f.name,f)
             temp = {"name":file,"url":"local_storage","size":fs.size(file)}
-            # temp["name"] = file
-            # temp["url"] = "local_storage"
-            # temp["size"] = fs.size(file)
             all_local_files.append(temp)
 
         file_serializer = DocSerializer(data=all_local_files, many=True)
         if file_serializer.is_valid():
             file_serializer.save()
-            #saving metadata of selected files in models
+            # saving metadata of selected files in models
             return redirect("/doclib/files_display")
-            #rendering HTML Page for displaying selected files
+            # rendering HTML Page for displaying selected files
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -160,49 +158,43 @@ class login_dm(APIView):
     view for signing in the digimocker user account
     '''
     def get(self, request):
+        # rendering HTML Form Page for user to login digimocker
         return render(request,'login_digimocker.html')
-        #rendering HTML Form Page for user to login digimocker
 
     def post(self, request):
         user_email = request.POST.get('email')
         user_password = request.POST.get('password')
         query = {'email':user_email, 'password':user_password}
+        # making a login POST request to digimocker api
         response = requests.post('https://digimocker.herokuapp.com/api/user/login', json=query)
-        #making a login POST request to digimocker api
 
         if response.status_code != 200:
             return HttpResponse(status=response.status_code)
-        print("LOGIN: ",response.status_code)
         authToken = str(response.text)
-        print("AUTH",authToken)
-        #setting auth-token on successful user login to digimocker
-
+        # setting auth-token on successful user login to digimocker
         request.session['token'] = authToken
         request.session['email'] = user_email
         my_headers = {"auth-token": authToken}
         response = requests.post('https://digimocker.herokuapp.com/api/docs', json={"email":user_email},headers=my_headers)
-        #making a GET request to digimocker api documents
+        # making a GET request to digimocker api documents
+        
         if response.status_code != 200:
             return HttpResponse(status=response.status_code)
         data=[]
-        print("GET FILES: ",response.status_code)
-
         if(response.text):
             data=json.loads(response.text)
             for jobj in data:
                 jobj['fid'] = jobj.pop('_id')
-
+        # rendering HTML Page for displaying user's digimocker files
         return render(request,'showDigimockerFiles.html',{'files':data})
-        #rendering HTML Page for displaying user's digimocker files
-
-
+        
 class register_dm(APIView):
     '''
     view for registering as a new user in digimocker
     '''
     def get(self, request):
+        # rendering HTML Form Page for user to register digimocker
         return render(request,'register_digimocker.html')
-        #rendering HTML Form Page for user to register digimocker
 
     def post(self, request):
         user_name=request.POST.get('name')
@@ -210,13 +202,12 @@ class register_dm(APIView):
         user_password=request.POST.get('password')
         query = {"name":user_name,"email":user_email, "password":user_password}
         response = requests.post('https://digimocker.herokuapp.com/api/user/register', json=query)
-        #making a login POST request to digimocker api
-
-        print("REGISTER: ",response.status_code)
+        # making a login POST request to digimocker api
+        
         if response.status_code != 200:
             return HttpResponse(status=response.status_code)
+        # rendering HTML Form Page for user to login digimocker on successful register
         return redirect("/doclib/login_digimocker")
-        #rendering HTML Form Page for user to login digimocker on successful register
 
 
 class upload_to_dm(APIView):
@@ -224,9 +215,9 @@ class upload_to_dm(APIView):
     view for uploading documents to user's digimocker account
     '''
     def get(self, request):
+        # rendering HTML Form Page for user to upload new doc to digimocker
         return render(request,'uploadToDm.html')
-        #rendering HTML Form Page for user to upload new doc to digimocker
-
+        
     def post(self, request):
         my_headers = {'auth-token' : request.session['token']}
         # receiving auth-token from session variable
@@ -238,15 +229,14 @@ class upload_to_dm(APIView):
 
         file_data={"name":name,"email":email,"identifier":identifier,"url":url}
         response = requests.post('https://digimocker.herokuapp.com/api/add', json=file_data,headers=my_headers)
-        #making a POST request to digimocker api for uploading new doc
+        # making a POST request to digimocker api for uploading new doc
 
         response = requests.post('https://digimocker.herokuapp.com/api/docs', json={"email":user_email},headers=my_headers)
-        #making a GET request to digimocker api documents
+        # making a GET request to digimocker api documents
 
         data=json.loads(response.text)
+        # rendering HTML Page for displaying user's digimocker files
         return render(request,'showDigimockerFiles.html',{'files':data})
-        #rendering HTML Page for displaying user's digimocker files
-
 
 class upload_from_dm(APIView):
     '''
@@ -254,14 +244,14 @@ class upload_from_dm(APIView):
     if user has selected digimocker as a data source
     '''
     def post(self, request):
+        # getting digimocker selected files metadata from checkbox response
         selected_files=request.POST.getlist('selected')
-        #getting digimocker selected files metadata from checkbox response
-
+        
         data = []
         for i in range(0,len(selected_files)):
             file_name, file_url = selected_files[i].split(", ")
             r = requests.get(file_url)
-            #getting file using downloadable url received in file's metadata
+            # getting file using downloadable url received in file's metadata
 
             path = os.path.join(media_dir,str(file_name))
             with open(path,'wb') as f:
@@ -271,23 +261,23 @@ class upload_from_dm(APIView):
             data.append(temp)
         serializer=DocSerializer(data=data, many=True)
         if serializer.is_valid():
+            # saving metadata of selected files in models
             serializer.save()
-            #saving metadata of selected files in models
+            # redirecting to files display page
             return redirect("/doclib/files_display")
-            #redirecting to files display page
-
-
+            
 def render_files(request):
     '''
     view for displaying selected files from various
     data sources to the user for final upload with
     an option to delete any file as per requirement
     '''
+    # getting files metadata from models, sequenced in reverse order of their size
     files_obj=fileDoc.objects.all().order_by('-size')
-    #getting files metadata from models, sequenced in reverse order of their size
-
+    
+    # rendering to files display page
     return render(request,'source_display.html',{'files':files_obj})
-    #rendering to files display page
+    
 
 def delete_file(request, id):
     '''
@@ -297,12 +287,12 @@ def delete_file(request, id):
     item = fileDoc.objects.get(pk=id)
     path = os.path.join(media_dir, str(item))
     os.remove(path)
+    # deleting selected file from model as well as project local storage i.e. media (here)
     item.delete()
-    #deleting selected file from model as well as project local storage i.e. media (here)
 
+    # rendering to files display page
     return redirect("/doclib/files_display")
-    #rendering to files display page
-
+    
 def clear_user_data_from_app():
     '''
     clearing the files from database as well
@@ -331,14 +321,13 @@ def clear_user_data_from_app():
     globals()['gcs_client_json_file_name'] = None
 
 
-
 def upload_S3(request):
     '''
     view for uploading selected documents to amazon s3 bucket
     '''
+    # getting files metadata from models, sequenced in reverse order of their size
     files_obj=fileDoc.objects.all().order_by('-size')
-    #getting files metadata from models, sequenced in reverse order of their size
-
+    
     if(not len(files_obj)>0):
         return "error"
     info = {}
@@ -346,16 +335,15 @@ def upload_S3(request):
     info['object_token'] = globals()['object_token']
     info['access_id_received'] = globals()['access_id_received']
     info['secret_key_received'] = globals()['secret_key_received']
+    # uploading files to amazon s3 bucket with access information provided earlier
     return awsS3.upload(files_obj, info)
-    #uploading files to amazon s3 bucket with access information provided earlier
-
 
 def upload_gcs(request):
     '''
     view for uploading selected documents to google cloud bucket
     '''
+    # getting files metadata from models, sequenced in reverse order of their size
     files_obj = fileDoc.objects.all().order_by('-size')
-    #getting files metadata from models, sequenced in reverse order of their size
 
     if(not len(files_obj)>0):
         return "error"
@@ -363,9 +351,9 @@ def upload_gcs(request):
     info['bucket_received'] = globals()['bucket_received']
     info['object_token'] = globals()['object_token']
     info['gcs_client_json_file_name'] = globals()['gcs_client_json_file_name']
+    # uploading files to amazon gcs bucket with access information provided earlier
     return google_drive_gcs.upload(files_obj, info)
-    #uploading files to amazon gcs bucket with access information provided earlier
-
+    
 def upload(request):
     '''
     view for conditional selection of
@@ -380,15 +368,15 @@ def upload(request):
         result  = upload_S3(request)
 
     if(len(result)==0):
-        #no files selected by user or session gets terminated
+        # no files selected by user or session gets terminated
         clear_user_data_from_app()
         return render(request,'errors_page.html',{'error':'session out'})
 
     elif(result[0]==True):
-        #files get successfully uploaded
+        # files get successfully uploaded
         clear_user_data_from_app()
         return render(request,"storage_service.html",{'message':True})
     else:
-        #error occured while uploading
+        # error occured while uploading
         clear_user_data_from_app()
         return render(request,'errors_page.html',{'error':result[1],'credentials_error':True})
